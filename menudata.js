684ed79c804a97f7ed9e9b36a9ee8b65f541b1c4
AnjaCramer <77159687+AnjaCramer@users.noreply.github.com>
2021-12-21 14:08:23 +0000

    Deploying to gh-pages from  @ 2a923bfdc85bf81f9f7ee64e938b1fc8d0a1253e 🚀

--- menudata.js
+++ menudata.js
@@ -45,9 +45,7 @@ var menudata={children:[
 {text:"s",url:"namespacemembers.html#index_s"},
 {text:"t",url:"namespacemembers.html#index_t"},
 {text:"u",url:"namespacemembers.html#index_u"},
-{text:"v",url:"namespacemembers.html#index_v"},
-{text:"w",url:"namespacemembers.html#index_w"},
-{text:"x",url:"namespacemembers.html#index_x"}]},
+{text:"v",url:"namespacemembers.html#index_v"}]},
 {text:"Functions",url:"namespacemembers_func.html",children:[
 {text:"a",url:"namespacemembers_func.html#index_a"},
 {text:"c",url:"namespacemembers_func.html#index_c"},
@@ -57,7 +55,8 @@ var menudata={children:[
 {text:"h",url:"namespacemembers_func.html#index_h"},
 {text:"i",url:"namespacemembers_func.html#index_i"},
 {text:"m",url:"namespacemembers_func.html#index_m"},
-{text:"r",url:"namespacemembers_func.html#index_r"}]},
+{text:"r",url:"namespacemembers_func.html#index_r"},
+{text:"s",url:"namespacemembers_func.html#index_s"}]},
 {text:"Variables",url:"namespacemembers_vars.html",children:[
 {text:"a",url:"namespacemembers_vars.html#index_a"},
 {text:"b",url:"namespacemembers_vars.html#index_b"},
@@ -77,8 +76,6 @@ var menudata={children:[
 {text:"s",url:"namespacemembers_vars.html#index_s"},
 {text:"t",url:"namespacemembers_vars.html#index_t"},
 {text:"u",url:"namespacemembers_vars.html#index_u"},
-{text:"v",url:"namespacemembers_vars.html#index_v"},
-{text:"w",url:"namespacemembers_vars.html#index_w"},
-{text:"x",url:"namespacemembers_vars.html#index_x"}]}]}]},
+{text:"v",url:"namespacemembers_vars.html#index_v"}]}]}]},
 {text:"Files",url:"files.html",children:[
 {text:"File List",url:"files.html"}]}]}
