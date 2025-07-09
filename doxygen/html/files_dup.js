090774c9678896d505fb4735f8a2b5b3fe474147
situx <situx@users.noreply.github.com>
2025-07-09 13:23:14 +0000

    Deploying to gh-pages from @ i3mainz/3dcap-md-gen@96b6d3752c003b142e01cbb2683441608a46eeec 🚀

--- doxygen/html/files_dup.js
+++ doxygen/html/files_dup.js
@@ -1,7 +1,7 @@
 var files_dup =
 [
     [ "agisoft-metashape_3dcap-metadata.py", "a00011.html", "a00011" ],
-    [ "atos-2016_3dcap-metadata.py", "a00008.html", "a00008" ],
-    [ "atos-v62-txt2json.py", "a00002.html", "a00002" ],
-    [ "atos-v62_3dcap_metadata.py", "a00005.html", "a00005" ]
+    [ "atos-2016_3dcap-metadata.py", "a00002.html", "a00002" ],
+    [ "atos-v62-txt2json.py", "a00005.html", "a00005" ],
+    [ "atos-v62_3dcap_metadata.py", "a00008.html", "a00008" ]
 ];
